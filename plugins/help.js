21c72887abfc2c75362b775452d219011cd68ada
mrclfd <mrclfd@yahoo.com>
2021-04-13 02:44:06 +0700

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -3,6 +3,7 @@ Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
 */
 
+/*
 const Asena = require('../events');
 const {MessageType, MessageOptions} = require('@adiwajshing/baileys');
 const Config = require('../config');
@@ -122,3 +123,4 @@ else {
         }
     }));
 }
+*/
