ede994ba1281a15f26c18fdceab46ccd902091f0
mrclfd <mrclfd@yahoo.com>
2021-04-13 01:38:37 +0700

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -20,16 +20,16 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
-	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
+	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=6d91e6f72ccc2bfa5729e491e7cdb30a&lang=id`;
 	    try {
 		    const response = await got(url);
 		    const json = JSON.parse(response.body);
 		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
 		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
 		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
-		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
+		    '*☀ ' + Lang.HUMI +':* ```' + json.main.humidity + '%```\n' + 
 		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
-		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
+		    '*☁ ' + Lang.CLOUD +':* ```' + json.clouds.all + '%```\n', MessageType.text);
 	    } catch {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
@@ -37,19 +37,19 @@ if (Config.WORKTYPE == 'private') {
 }
 if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
+    Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
-	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
+	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=6d91e6f72ccc2bfa5729e491e7cdb30a&lang=id`;
 	    try {
 		    const response = await got(url);
 		    const json = JSON.parse(response.body);
 		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
 		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
 		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
-		    '*☀ ' + Lang.HUMI +':* ```%' + json.main.humidity + '```\n' + 
+		    '*☀ ' + Lang.HUMI +':* ```' + json.main.humidity + '%```\n' + 
 		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
-		    '*☁ ' + Lang.CLOUD +':* ```%' + json.clouds.all + '```\n', MessageType.text);
+		    '*☁ ' + Lang.CLOUD +':* ```' + json.clouds.all + '%```\n', MessageType.text);
 	    } catch {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
