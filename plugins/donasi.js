09a985d0165ca86580ab325809633a139a3f8a80
mrclfd <mrclfd@yahoo.com>
2021-04-13 13:34:32 +0700

    Create donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -11,7 +11,7 @@ const fs = require('fs');
       
           var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\n      Untuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas.\n     Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\n          Untuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas.\n\n        Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
       
         await new Promise(r => setTimeout(r, 1650));
   
@@ -25,7 +25,7 @@ const fs = require('fs');
       
           var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
        
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\n      Untuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas.\n     Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\n         Untuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas.\n\n        Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
       
         await new Promise(r => setTimeout(r, 1650));
   
