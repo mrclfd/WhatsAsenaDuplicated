910df9afb29cce40f501d7d5c77925c56ad0669f
mrclfd <mrclfd@yahoo.com>
2021-04-13 09:37:50 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -9,8 +9,8 @@ const fs = require('fs');
   
     Asena.addCommand({pattern: 'donation', fromMe: true}, (async (message, match) => {
       
-          var image = await axios.get ('https://saweria.co/overlays/qr?streamKey=07bbe0821c279ce665e4f0745c29dd86&backgroundColor=%23f8e71cFF&barcodeColor=%23000000FF&username=mrclfd', {responseType: 'arraybuffer'})
-       await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg, caption: `╭─「 Donasi-nya Kakak 」
+          var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
+       await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭─「 Donasi-nya Kakak 」
 │ • Saweria [saweria.co/mrclfd]
 ╰────`})
       
@@ -24,8 +24,8 @@ const fs = require('fs');
   
     Asena.addCommand({pattern: 'donation', fromMe: false}, (async (message, match) => {
       
-          var image = await axios.get ('https://saweria.co/overlays/qr?streamKey=07bbe0821c279ce665e4f0745c29dd86&backgroundColor=%23f8e71cFF&barcodeColor=%23000000FF&username=mrclfd', {responseType: 'arraybuffer'})
-       await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.jpg, caption: `╭─「 Donasi-nya Kakak 」
+          var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
+       await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭─「 Donasi-nya Kakak 」
 │ • Saweria [saweria.co/mrclfd]
 ╰────`})
       
