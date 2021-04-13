9d4f2323cfdd72711cb9ef71e31f7e49c06dad47
mrclfd <mrclfd@yahoo.com>
2021-04-13 10:22:45 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -7,7 +7,7 @@ const fs = require('fs');
   
   if (con.WORKTYPE == 'private') {
   
-    Asena.addCommand({pattern: 'donation', fromMe: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'donasi$', fromMe: true}, (async (message, match) => {
       
           var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\nUntuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas. Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
@@ -20,7 +20,7 @@ const fs = require('fs');
   }
   else if (con.WORKTYPE == 'public') {
   
-    Asena.addCommand({pattern: 'donation', fromMe: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'donasi$', fromMe: true}, (async (message, match) => {
       
           var image = await axios.get ('https://d.top4top.io/p_1929me13r1.png', {responseType: 'arraybuffer'})
        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Donasi Seikhlasnya*:\n\nUntuk saat ini kami hanya tersedia metode donasi lewat saweria secara langsung, dan ini (saweria.co/mrclfd) link halaman saweria kami atau bisa juga dengan meng-scan kode qr diatas. Semoga dengan rejeki yang kalian bagikan dapat bermanfaat bagi kami, mungkin nanti akan dibelikan api key/lainnya agar berguna juga bagi Bot :)`})
