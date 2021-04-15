8fc6bffb9d355d2719028e9585b46da74f74da3d
mrclfd <mrclfd@yahoo.com>
2021-04-15 10:45:12 +0700

    Update adzan.js

--- plugins/adzan.js
+++ plugins/adzan.js
@@ -1,6 +1,6 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const axios = require('axios');
+const got = require('got');
 const Config = require('../config');
 
 const Language = require('../language');
@@ -8,37 +8,25 @@ const Lang = Language.getString('adzan');
 
 if (Config.WORKTYPE == 'private') {
 
-Asena.addCommand({ pattern: 'adzan ?(.*)', fromMe: true, desc: Lang.ADZAN_DESC }, async (message, match) => {
+        Asena.addCommand({pattern: 'adzan ?(.*)', desc: Lang.ADZAN_DESC, fromMe: true}, async (message, match) => {
 
-        const Daerah = match[1]
-
-        await axios
-          .get(`https://docs-jojo.herokuapp.com/api/jadwalshalat?daerah=${Daerah}`)
-          .then(async (response) => {
-            const {
-              Imsyak,
-              Subuh,
-              Dhuha,
-              Dzuhur,
-              Ashar,
-              Maghrib,
-              Isya,
-            } = response.data
-
-            const msg = `📍 *Lokasi:* ${Daerah}
-
-*Imsak:* ${Imsyak}
-*Subuh:* ${Subuh}
-*Dhuha:* ${Dhuha}
-*Dzuhur:* ${Dzuhur}
-*Ashar:* ${Ashar}
-*Maghrib:* ${Maghrib}
-*Isya:* ${Isya}`
-
-            await message.client.sendMessage(message.jid, msg, MessageType.text)
-           })
-      },
-    )
+	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
+	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n' +
+                    '*Waktu :* ```' + json.datetime[0].date.gregorian + '```\n\n' +
+		    '*- Imsak:* ```' + json.datetime[0].times.Imsak + '```\n' + 
+		    '*- Subuh:* ```' + json.datetime[0].times.Fajr + '```\n' +
+		    '*- Dzuhur:* ```' + json.datetime[0].times.Dhuhr + '```\n' + 
+		    '*- Ashar:* ```' + json.datetime[0].times.Asr + '```\n' + 
+                    '*- Maghrib:* ```' + json.datetime[0].times.Maghrib + '```\n' + 
+		    '*- Isya:* ```' + json.datetime[0].times.Isha + '```\n', MessageType.text);
+	    } catch {
+		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	    }
+    });
 }
 
 if (Config.WORKTYPE == 'public') {
