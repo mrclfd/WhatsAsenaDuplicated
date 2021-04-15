7c61694558d3f99f67c83f7b16918fd89bef7e1d
mrclfd <mrclfd@yahoo.com>
2021-04-15 08:46:44 +0700

    Create brainly.js

---
+++ plugins/brainly.js
@@ -0,0 +1,52 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('brainly');
+
+if (Config.WORKTYPE == 'private') {
+
+Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: true, desc: Lang.BRAINLY_DESC }, async (message, match) => {
+
+        const Soal = match[1]
+
+        await axios
+          .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
+          .then(async (response) => {
+            const {
+              soal,
+              jawaban,
+            } = response.data
+
+            const msg = `*BRAINLY*\n\n
+SOAL: ${soal}
+JAWABAN:\n${jawaban}`
+            await message.client.sendMessage(message.jid, msg, MessageType.text)
+           })
+      },
+    )
+}
+
+if (Config.WORKTYPE == 'public') {
+  
+  Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: true, desc: Lang.BRAINLY_DESC }, async (message, match) => {
+
+        const Soal = match[1]
+
+        await axios
+          .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
+          .then(async (response) => {
+            const {
+              soal,
+              jawaban,
+            } = response.data
+
+            const msg = `*BRAINLY*\n\n
+SOAL: ${soal}
+JAWABAN:\n${jawaban}`
+            await message.client.sendMessage(message.jid, msg, MessageType.text)
+           })
+      },
+    )
+}
