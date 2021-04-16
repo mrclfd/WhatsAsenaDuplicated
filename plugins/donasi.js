5f959c952b5e60c16c00a79852aa8d9a4637bf7c
mrclfd <mrclfd@yahoo.com>
2021-04-16 11:22:32 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -9,7 +9,7 @@ const Lang = Language.getString('donasi');
   
   if (con.WORKTYPE == 'private') {
   
-    Asena.addCommand({pattern: 'donasi$', fromMe: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'donasi$', fromMe: true, desc: Lang.DONASI_DESC }, (async (message, match) => {
       
           var image = await axios.get ('https://raw.githubusercontent.com/mrclfd/WhatsAsenaDuplicated/alpha-test/media/image/image-i.png', {responseType: 'arraybuffer'})
        
@@ -25,13 +25,13 @@ const Lang = Language.getString('donasi');
   }
   else if (con.WORKTYPE == 'public') {
   
-    Asena.addCommand({pattern: 'donasi$', fromMe: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'donasi$', fromMe: true, desc: Lang.DONASI_DESC }, (async (message, match) => {
       
           var image = await axios.get ('https://raw.githubusercontent.com/mrclfd/WhatsAsenaDuplicated/alpha-test/media/image/image-i.png', {responseType: 'arraybuffer'})
        
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `Buy Me a Coffee\nvia saweria.co/budilasidodo`})
       
-     /*  
+     /*
         await new Promise(r => setTimeout(r, 1650));
   
         await message.sendMessage('Thx u <3');
