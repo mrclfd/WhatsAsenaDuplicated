4a825cd6cbec967d777647cbdc6b137a13eed804
mrclfd <mrclfd@yahoo.com>
2021-04-19 09:38:01 +0700

    :D

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ module.exports = {
     NO_WA: process.env.ALIVE_NUMBER === undefined ? 'unknown' : process.env.ALIVE_NUMBER,
     USER_WA: process.env.ALIVE_NAME === undefined ? 'unknown' : process.env.ALIVE_NAME,
     LH_API: process.env.LH_API_KEY === undefined ? 'AUFUH724T9AH821ASC' : process.env.LH_API_KEY,
-    BRANCH: 'latest',
+    BRANCH: 'master',
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
