c28773087e8f0a8e75143aef6004734949d961c1
mrclfd <mrclfd@yahoo.com>
2021-04-21 05:48:37 +0700

    try add hidetag and tagadmin from Xenon67

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -11,6 +11,7 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
+
 if (Config.WORKTYPE == 'private') {
 
 Asena.addCommand({pattern: 'tagall$', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
@@ -27,4 +28,79 @@ Asena.addCommand({pattern: 'tagall$', fromMe: true, desc: Lang.TAGALL_DESC}, (as
     await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
 }));
 
+Asena.addCommand({pattern: 'hidetag ?(.*)', fromMe: true, desc: Lang.HIDETAG_DESC}, (async (message, match) => {
+
+    if (match[1] !== '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,`${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (match[1] == '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+}));
+const rp = "*Melaporkan!*"
+const ss = "Admin"
+Asena.addCommand({pattern: 'tagadmin', fromMe: true, desc: ss}, (async (message, match) => {
+    let grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(async (uye) => {
+        if (uye.isAdmin) {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+        }
+    });
+    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
+
+const tor = "Ini mengirimkan laporan ke admin."
+const rep = "*Harap balas Pesan Pengguna untuk Dilaporkan!*"
+const rap = "=== ```Lapor``` ===\n\n"
+Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: tor}, (async (message, match) => {
+    if (match[1] == '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Pengguna:* ' + '@' + message.reply_message.jid.split('@')[0] , MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        
+    }
+    else if (match[1] !== '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Pengguna:* ' + '@' + message.reply_message.jid.split('@')[0] + `\n*Sebep:* ${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (!message.reply_message) {
+        return message.client.sendMessage(message.jid,rep, MessageType.text);
+    }
+}));
 }
