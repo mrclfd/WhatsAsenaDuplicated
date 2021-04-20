e152a7264d310a54512ebcb49ff20f9e729decc0
mrclfd <mrclfd@yahoo.com>
2021-04-20 15:27:36 +0700

    try to improve again

--- plugins/paste.js
+++ plugins/paste.js
@@ -27,7 +27,7 @@ if (Config.WORKTYPE == 'private') {
             const response = await got.post(base_URI, {json : {content : message.reply_message.text}}); 
             json = JSON.parse(response.body);
             neko_url = 'https://nekobin.com/' + json.result.key;
-            await message.reply(`Berhasil ditempel!\n\n[Nekobin URL](${neko_url})\n[Lihat RAW](https://nekobin.com/raw/${json.result.key})`);        
+            await message.reply('*Berhasil ditempel!*\n\n```Nekobin URL:```'+neko_url+'\n```Lihat RAW:```https://nekobin.com/raw/'+json.result.key+'');        
         } catch (err) {
             await message.reply(err.message, MessageType.text);
             console.log(err.message);
