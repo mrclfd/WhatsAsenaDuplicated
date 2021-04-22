386b6f9b3dbf83f7ced00f9bb0f366c8b7683a30
mrclfd <mrclfd@yahoo.com>
2021-04-22 07:16:06 +0700

    y

--- plugins/ffmpeg.js
+++ plugins/ffmpeg.js
@@ -18,7 +18,7 @@ if (Config.WORKTYPE == 'private') {
 
     Asena.addCommand({pattern: 'ffmpeg ?(.*)', fromMe: true, desc: Lang.FF_DESC}, (async (message, match) => {
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Media and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg curves=vintage, fps=fps=25```', MessageType.text);
+        if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Media and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg curves=vintage || fps=25```', MessageType.text);
         if (message.reply_message.video) {
 
             var downloading = await message.client.sendMessage(message.jid,Lang.FF_PROC,MessageType.text);
@@ -82,7 +82,7 @@ else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'ffmpeg ?(.*)', fromMe: false, desc: Lang.FF_DESC}, (async (message, match) => {
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Media and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg curves=vintage, fps=fps=25```', MessageType.text);
+        if (match[1] === '') return await message.client.sendMessage(message.jid,'Need Media and Filter Name!\nℹ️ Ex: ```.ffmpeg fade=in:0:30```\nℹ️ Ex: ```.ffmpeg curves=vintage || fps=25```', MessageType.text);
         if (message.reply_message.video) {
 
             var downloading = await message.client.sendMessage(message.jid,Lang.FF_PROC,MessageType.text);
