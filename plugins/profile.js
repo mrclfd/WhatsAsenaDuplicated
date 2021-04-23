06e904c78537b4292461af2a07bd6bb35c9fdc76
mrclfd <mrclfd@yahoo.com>
2021-04-23 14:36:33 +0700

    wame: try to fix

--- plugins/profile.js
+++ plugins/profile.js
@@ -104,17 +104,15 @@ Asena.addCommand({pattern: 'unblock ?(.*)', fromMe: true, desc: Lang.UNBLOCK_DES
     Asena.addCommand({pattern: 'wame ?(.*)', fromMe: true, desc: Lang.WAME_DESC}, (async (message, match) => {    
         if (message.reply_message !== false) {
             await message.client.sendMessage(message.jid, Lang.WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid), MessageType.text, {
-                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', ' ')]}
             });
         } else if (message.mention !== false) {
             message.mention.map(async user => {
                 await message.client.sendMessage(message.jid, Lang.WAME.format(user.split('@')[0], user), MessageType.text, {
-                    contextInfo: {mentionedJid: [user.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                    contextInfo: {mentionedJid: [user.replace('c.us', ' ')]}
                 });    
             });
-        } else {
-            await message.client.sendMessage(message.jid, Lang.WAME_CHAT.format(message.jid), MessageType.text);
-        }
+        } 
     }));
 }
 else if (Config.WORKTYPE == 'public') {
@@ -135,20 +133,18 @@ else if (Config.WORKTYPE == 'public') {
         }
     }));
     
-    Asena.addCommand({pattern: 'wame ?(.*)', fromMe: false, desc: Lang.WAME_DESC}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'wame ?(.*)', fromMe: true, desc: Lang.WAME_DESC}, (async (message, match) => {    
         if (message.reply_message !== false) {
             await message.client.sendMessage(message.jid, Lang.WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid), MessageType.text, {
-                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', ' ')]}
             });
         } else if (message.mention !== false) {
             message.mention.map(async user => {
                 await message.client.sendMessage(message.jid, Lang.WAME.format(user.split('@')[0], user), MessageType.text, {
-                    contextInfo: {mentionedJid: [user.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                    contextInfo: {mentionedJid: [user.replace('c.us', ' ')]}
                 });    
             });
-        } else {
-            await message.client.sendMessage(message.jid, Lang.WAME_CHAT.format(message.jid), MessageType.text);
-        }
+        } 
     }));
 }
 
