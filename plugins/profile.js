fa31b90dce0c678295493195213a8d26054c5818
mrclfd <mrclfd@yahoo.com>
2021-04-23 15:08:12 +0700

    wame: try to fix again

--- plugins/profile.js
+++ plugins/profile.js
@@ -104,12 +104,12 @@ Asena.addCommand({pattern: 'unblock ?(.*)', fromMe: true, desc: Lang.UNBLOCK_DES
     Asena.addCommand({pattern: 'wame ?(.*)', fromMe: true, desc: Lang.WAME_DESC}, (async (message, match) => {    
         if (message.reply_message !== false) {
             await message.client.sendMessage(message.jid, Lang.WAME.format(message.reply_message.jid.split('@')[0], message.reply_message.jid), MessageType.text, {
-                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
             });
         } else if (message.mention !== false) {
             message.mention.map(async user => {
-                await message.client.sendMessage(message.jid, Lang.WAME.format(user.split('@')[0], user), MessageType.text, {
-                    contextInfo: {mentionedJid: [user.replace('c.us', ' ').replace('s.whatsapp.net', ' ')]}
+                await message.client.sendMessage(message.jid, Lang.WAME.format(user), MessageType.text, {
+                    contextInfo: {mentionedJid: [user]}
                 });    
             });
         } else {
