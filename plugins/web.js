6e96966e9d4ef379320545067c09252aa9c6947c
mrclfd <mrclfd@yahoo.com>
2021-04-26 01:40:30 +0700

    ping: change ms calculation scale

--- plugins/web.js
+++ plugins/web.js
@@ -50,7 +50,7 @@ Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: false, desc: Lan
   var start = new Date().getTime();
   await message.sendMessage('```Ping!```');
   var end = new Date().getTime();
-  var duration = Math.floor((end - start) / 1000)
+  var duration = Math.floor((end - start) / 100)
   
   await message.client.sendMessage(
     message.jid,'*Pong!*\n```' + duration + 'ms```', MessageType.text);
@@ -59,11 +59,11 @@ Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: false, desc: Lan
 }
 else if (Config.WORKTYPE == 'public') {
     
-    Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: false, desc: Lang.PING_DESC}, (async (message, match) => {
+    Asena.addCommand({pattern: 'ping', fromMe: false, deleteCommand: false, desc: Lang.PING_DESC}, (async (message, match) => {
   var start = new Date().getTime();
   await message.sendMessage('```Ping!```');
   var end = new Date().getTime();
-  var duration = Math.floor((end - start) / 1000)
+  var duration = Math.floor((end - start) / 100)
   
   await message.client.sendMessage(
     message.jid,'*Pong!*\n```' + duration + 'ms```', MessageType.text);
