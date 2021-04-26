4fc79e8bea03e01b79ee6b9234d1c304f60d20e5
mrclfd <mrclfd@yahoo.com>
2021-04-26 10:48:55 +0700

    now private command can be run publicly

--- plugins/weather.js
+++ plugins/weather.js
@@ -54,4 +54,24 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+	
+// now private command can be run publicly
+	
+	Asena.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true, dontAddCommandList: true}, async (message, match) => {
+
+	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
+	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=6d91e6f72ccc2bfa5729e491e7cdb30a&lang=id`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '*📍 ' + Lang.LOCATION +':* ```' + match[1] + '```\n\n' +
+		    '*☀ ' + Lang.TEMP +':* ```' + json.main.temp_max + '°```\n' + 
+		    '*ℹ ' + Lang.DESC +':* ```' + json.weather[0].description + '```\n' +
+		    '*☀ ' + Lang.HUMI +':* ```' + json.main.humidity + '%```\n' + 
+		    '*💨 ' + Lang.WIND +':* ```' + json.wind.speed + 'm/s```\n' + 
+		    '*☁ ' + Lang.CLOUD +':* ```' + json.clouds.all + '%```\n', MessageType.text);
+	    } catch {
+		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	    }
+    });
 }
