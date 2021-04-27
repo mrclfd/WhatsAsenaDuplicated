3c522e0c4d1bca458a99ff55cb3e08de28126061
mrclfd <mrclfd@yahoo.com>
2021-04-27 15:03:45 +0700

    now private command can be run publicly

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -21,39 +21,35 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.xteam.xyz/attp?file&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
 
-    Asena.addCommand({ pattern: 'bttp ?(.*)', fromMe: true, desc: Lang.BTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'bttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.BTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp2?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
     
-    Asena.addCommand({ pattern: 'cttp ?(.*)', fromMe: true, desc: Lang.CTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'cttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.CTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp4?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
     
-    Asena.addCommand({ pattern: 'gttp ?(.*)', fromMe: true, desc: Lang.GTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'gttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.GTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp3?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
-
 
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
@@ -67,39 +63,78 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://api.xteam.xyz/attp?file&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
     
-    Asena.addCommand({ pattern: 'bttp ?(.*)', fromMe: false, desc: Lang.BTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'bttp ?(.*)', fromMe: false, dontAddCommandList: true, desc: Lang.BTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp2?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
     
-    Asena.addCommand({ pattern: 'cttp ?(.*)', fromMe: false, desc: Lang.CTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'cttp ?(.*)', fromMe: false, dontAddCommandList: true, desc: Lang.CTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp4?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
-
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
     }));
     
-    Asena.addCommand({ pattern: 'gttp ?(.*)', fromMe: false, desc: Lang.GTTP_DESC }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'gttp ?(.*)', fromMe: false, dontAddCommandList: true, desc: Lang.GTTP_DESC }, (async (message, match) => {
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
 
         var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp3?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
+
+    }));
+   
+   
+   // now private command can be run publicly
+
+   Asena.addCommand({ pattern: 'attp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.ATTP_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+
+        var ttinullimage = await axios.get(`https://api.xteam.xyz/attp?file&text=${match[1].replace(/Ö/g, "%C3%96").replace(/ö/g, "%C3%B6").replace(/ü/g, "%C3%BC").replace(/Ü/g, "%C3%9C").replace(/Ğ/g, "%C4%9E").replace(/ğ/g, "%C4%9F").replace(/ş/g, "%C5%9F").replace(/Ş/g, "%C5%9E").replace(/ç/g, "%C3%A7").replace(/Ç/g, "%C3%87").replace(/ı/g, "%C4%B1").replace(/i/g, "%69").replace(/"/g, "%22").replace(/İ/g, "%C4%B0")}`, { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
+
+    }));
+
+    Asena.addCommand({ pattern: 'bttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.BTTP_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+
+        var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp2?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
+
+    }));
+    
+    Asena.addCommand({ pattern: 'cttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.CTTP_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+
+        var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp4?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
+
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
+
+    }));
+    
+    Asena.addCommand({ pattern: 'gttp ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.GTTP_DESC }, (async (message, match) => {
+
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+
+        var ttinullimage = await axios.get('https://api.lolhuman.xyz/api/ttp3?apikey='+Config.LH_API+'&text='+match[1]+'', { responseType: 'arraybuffer' })
 
         await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.sticker, { mimetype: Mimetype.webp })
 
