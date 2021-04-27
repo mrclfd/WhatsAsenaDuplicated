ebf30d19b6d1c29327f48f7e1d94b9e52913717d
mrclfd <mrclfd@yahoo.com>
2021-04-27 15:11:34 +0700

    now private command can be run publicly

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -104,3 +104,98 @@ Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, desc: tor}, (async (mes
     }
 }));
 }
+
+if (Config.WORKTYPE == 'public') {
+
+// now private command can be run publicly
+    
+Asena.addCommand({pattern: 'tagall$', fromMe: true, dontAddCommandList: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+
+    grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(
+        async (uye) => {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+        }
+    );
+    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
+
+Asena.addCommand({pattern: 'hidetag ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.HIDETAG_DESC}, (async (message, match) => {
+
+    if (match[1] !== '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,`${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (match[1] == '') {
+        grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(
+            async (uye) => {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        );
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+}));
+const rp = "*Melaporkan!*"
+const ss = "Admin"
+Asena.addCommand({pattern: 'tagadmin', fromMe: true, dontAddCommandList: true, desc: ss}, (async (message, match) => {
+    let grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(async (uye) => {
+        if (uye.isAdmin) {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+        }
+    });
+    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
+
+const tor = "Ini mengirimkan laporan ke admin."
+const rep = "*Harap balas Pesan Pengguna untuk Dilaporkan!*"
+const rap = "=== ```Lapor``` ===\n\n"
+Asena.addCommand({pattern: 'report ?(.*)', fromMe: true, dontAddCommandList: true, desc: tor}, (async (message, match) => {
+    if (match[1] == '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Pengguna:* ' + '@' + message.reply_message.jid.split('@')[0] , MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+        
+    }
+    else if (match[1] !== '' && message.reply_message) {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '@' + uye.id.split('@')[0] + ' ';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,rap + '*Pengguna:* ' + '@' + message.reply_message.jid.split('@')[0] + `\n*Alasan:* ${match[1]}`, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }
+    else if (!message.reply_message) {
+        return message.client.sendMessage(message.jid,rep, MessageType.text);
+    }
+}));
+}
