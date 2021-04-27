8f71fbaff4a79b5b00c1c7829a7607dbfeef9747
mrclfd <mrclfd@yahoo.com>
2021-04-27 14:52:18 +0700

    now private command can be run publicly

--- plugins/updater.js
+++ plugins/updater.js
@@ -95,3 +95,83 @@ Asena.addCommand({pattern: 'update now$', fromMe: true, desc: Lang.UPDATE_NOW_DE
     }
 }));
 }
+
+
+// now private command can be run publicly
+
+if (Config.WORKTYPE == 'public') {
+
+Asena.addCommand({pattern: 'update$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        await message.client.sendMessage(
+            message.jid,
+            Lang.UPDATE, MessageType.text
+        );    
+    } else {
+        var degisiklikler = Lang.NEW_UPDATE;
+        commits['all'].map(
+            (commit) => {
+                degisiklikler += '🔹 [' + commit.date.substring(0, 10) + ']: ' + commit.message + ' <rapiUp>\n';
+            }
+        );
+        
+        await message.client.sendMessage(
+            message.jid,
+            degisiklikler + '```', MessageType.text
+        ); 
+    }
+}));
+
+Asena.addCommand({pattern: 'update now$', fromMe: true, dontAddCommandList: true, desc: Lang.UPDATE_NOW_DESC, dontAddCommandList: true}, (async (message, match) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
+    if (commits.total === 0) {
+        return await message.client.sendMessage(
+            message.jid,
+            Lang.UPDATE, MessageType.text
+        );    
+    } else {
+        var guncelleme = await message.reply(Lang.UPDATING);
+        if (Config.HEROKU.HEROKU) {
+            try {
+                var app = await heroku.get('/apps/' + Config.HEROKU.APP_NAME)
+            } catch {
+                return await message.client.sendMessage(
+                    message.jid,Lang.INVALID_HEROKU, MessageType.text);
+            }
+
+            git.fetch('upstream', Config.BRANCH);
+            git.reset('hard', ['FETCH_HEAD']);
+
+            var git_url = app.git_url.replace(
+                "https://", "https://api:" + Config.HEROKU.API_KEY + "@"
+            )
+            
+            try {
+                await git.addRemote('heroku', git_url);
+            } catch { console.log('heroku remote ekli'); }
+            await git.push('heroku', Config.BRANCH);
+
+            await message.client.sendMessage(
+                message.jid,Lang.UPDATED, MessageType.text);
+
+            await message.sendMessage(Lang.AFTER_UPDATE);
+            
+        } else {
+            git.pull((async (err, update) => {
+                if(update && update.summary.changes) {
+                    await message.client.sendMessage(
+                        message.jid,Lang.UPDATED_LOCAL, MessageType.text);
+                    exec('npm install').stderr.pipe(process.stderr);
+                } else if (err) {
+                    await message.client.sendMessage(
+                            message.jid,'*❌ Pembaruan gagal!*\n*Error:* ```' + err + '```', MessageType.text);
+                }
+            }));
+            await guncelleme.delete();
+        }
+    }
+}));
+}
