7bcfeb2d66571c52b01ba6f54bad74ff12fd361d
mrclfd <mrclfd@yahoo.com>
2021-04-29 16:29:14 +0700

    now private command can be run publicly

--- plugins/paste.js
+++ plugins/paste.js
@@ -87,4 +87,41 @@ else if (Config.WORKTYPE == 'public') {
         }
        
     }));
+    
+    
+    // now private command can be run publicly
+    
+    Asena.addCommand({pattern: 'paste$', fromMe: true, dontAddCommandList: true, desc: Lang.NEKOBIN_DESC}, (async (message, match) => {
+
+        if (!message.reply_message) return await message.sendMessage(Lang.NEED_REPLY);
+        if (!message.reply_message.text) return await message.sendMessage(Lang.MUST_TEXT);
+        let base_URI = "https://nekobin.com/api/documents";
+        try {
+            const response = await got.post(base_URI, {json : {content : message.reply_message.text}}); 
+            json = JSON.parse(response.body);
+            neko_url = 'https://nekobin.com/' + json.result.key;
+            await message.reply('*Berhasil ditempel!*\n\n```Nekobin URL:```'+neko_url+'\n```Lihat RAW:```https://nekobin.com/raw/'+json.result.key+'');        
+        } catch (err) {
+            await message.reply(err.message, MessageType.text);
+            console.log(err.message);
+        }
+       
+    }));
+    
+    Asena.addCommand({pattern: 'paste d$', fromMe: true, dontAddCommandList: true, desc: Lang.DOGBIN_DESC}, (async (message, match) => {
+
+        if (!message.reply_message) return await message.sendMessage(Lang.NEED_REPLY);
+        if (!message.reply_message.text) return await message.sendMessage(Lang.MUST_TEXT);
+        let base_URI = "https://del.dog/documents";
+        try {
+            const response = await got.post(base_URI, {json : {content : message.reply_message.text}}); 
+            json = JSON.parse(response.body);
+            dogbin_url = 'https://del.dog/' + json.key;
+            await message.reply('*Berhasil ditempel!*\n\n```Dogbin URL:```'+dogbin_url+'\n```Lihat RAW:```https://del.dog/raw/'+json.key+'');        
+        } catch (err) {
+            await message.reply(err.message, MessageType.text);
+            console.log(err.message);
+        }
+       
+    }));
 }
