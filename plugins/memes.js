3d742818d8be24b4186f84ccd88a636c897a7036
mrclfd <mrclfd@yahoo.com>
2021-04-30 07:07:51 +0700

    now private command can be run publicly

--- plugins/memes.js
+++ plugins/memes.js
@@ -49,7 +49,7 @@ if (Config.WORKTYPE == 'private') {
             bottomText: bottomText,
         }, async function(err) {
             if(err) throw new Error(err)
-            await message.client.sendMessage(message.jid, fs.readFileSync('meme.png'), MessageType.image, {filename: 'meme.png', mimetype: Mimetype.png, caption: ' '});
+            await message.client.sendMessage(message.jid, fs.readFileSync('meme.png'), MessageType.image, {filename: 'meme.png', mimetype: Mimetype.png, caption: 'Meme maker - BOT'});
             await info.delete();    
         });
     }));
@@ -87,7 +87,46 @@ else if (Config.WORKTYPE == 'public') {
             bottomText: bottomText,
         }, async function(err) {
             if(err) throw new Error(err)
-            await message.client.sendMessage(message.jid, fs.readFileSync('meme.png'), MessageType.image, {filename: 'meme.png', mimetype: Mimetype.png, caption: ' '});
+            await message.client.sendMessage(message.jid, fs.readFileSync('meme.png'), MessageType.image, {filename: 'meme.png', mimetype: Mimetype.png, caption: 'Meme maker - BOT'});
+            await info.delete();    
+        });
+    }));
+	
+	
+	// now private command can be run publicly
+	
+	Asena.addCommand({pattern: 'meme ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.MEMES_DESC}, (async (message, match) => {   
+
+        if (message.reply_message === false) return await message.client.sendMessage(message.jid,Lang.NEED_REPLY, MessageType.text);
+        var topText, bottomText;
+        if (match[1].includes(';')) {
+            var split = match[1].split(';');
+            topText = split[0];
+            bottomText = split[1];
+        }
+	    else {
+            topText = match[1];
+            bottomText = '';
+        }
+    
+	    var info = await message.reply(Lang.DOWNLOADING);
+	
+        var location = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        }); 
+    
+	    memeMaker({
+            image: location,         
+            outfile: 'meme.png',
+            topText: topText,
+            bottomText: bottomText,
+        }, async function(err) {
+            if(err) throw new Error(err)
+            await message.client.sendMessage(message.jid, fs.readFileSync('meme.png'), MessageType.image, {filename: 'meme.png', mimetype: Mimetype.png, caption: 'Meme maker - BOT'});
             await info.delete();    
         });
     }));
