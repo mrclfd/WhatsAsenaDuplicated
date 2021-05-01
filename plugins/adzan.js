c1cae73a9861953498234b38fd11a36328741a31
mrclfd <mrclfd@yahoo.com>
2021-05-01 13:24:39 +0700

    now private command can be run publicly

--- plugins/adzan.js
+++ plugins/adzan.js
@@ -58,4 +58,28 @@ if (Config.WORKTYPE == 'public') {
 		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
 	    }
     });
+	
+
+// now private command can be run publicly
+	
+  Asena.addCommand({pattern: 'adzan ?(.*)', dontAddCommandList: true, desc: Lang.ADZAN_DESC, fromMe: true}, async (message, match) => {
+
+	    if (match[1] === '') return await message.reply('*Masukkan kota!*');
+	    const url = `https://api.pray.zone/v2/times/today.json?city=${match[1]}`;
+	    try {
+		    const response = await got(url);
+		    const json = JSON.parse(response.body);
+		    if (response.statusCode === 200) return await message.client.sendMessage(message.jid,  '*Jadwal Sholat*:\n' +
+                    '📅 ```' + json.results.datetime[0].date.gregorian + ' | ' + json.results.location.timezone + '```\n' +
+                    '🌏 ```' + json.results.location.city + ' | ' + json.results.location.country + '```\n\n' +
+		    '```Imsak   :``` *' + json.results.datetime[0].times.Imsak + '*\n' + 
+		    '```Subuh   :``` *' + json.results.datetime[0].times.Fajr + '*\n' +
+		    '```Dzuhur  :``` *' + json.results.datetime[0].times.Dhuhr + '*\n' + 
+		    '```Ashar   :``` *' + json.results.datetime[0].times.Asr + '*\n' + 
+		    '```Maghrib :``` *' + json.results.datetime[0].times.Maghrib + '*\n' +
+                    '```Isya    :``` *' + json.results.datetime[0].times.Isha + '*\n', MessageType.text);
+	    } catch {
+		    return await message.client.sendMessage(message.jid, Lang.NOT_FOUND, MessageType.text);
+	    }
+    });
 }
