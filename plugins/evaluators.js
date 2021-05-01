337557cc9ba42f4530970164f00663b05cb66f77
mrclfd <mrclfd@yahoo.com>
2021-05-01 12:59:05 +0700

    now private command can be run publicly

--- plugins/evaluators.js
+++ plugins/evaluators.js
@@ -30,3 +30,23 @@ Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, desc: Lang.TERM_DESC}, (a
       });
 }));
 }
+
+
+if (Config.WORKTYPE == 'public') {
+    
+    
+// now private command can be run publicly
+
+Asena.addCommand({pattern: 'term ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.TERM_DESC}, (async (message, match) => {    
+    var user = os.userInfo().username;
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.GIVE_ME_CODE,MessageType.text);
+
+    exec(match[1], async (err, stdout, stderr) => {
+        if (err) {
+            return await message.client.sendMessage(message.jid,'```' + user + ':~# ' + match[1] + '\n' + err + '```',MessageType.text);
+        }
+        
+        return await message.client.sendMessage(message.jid,'```' + user + ':~# ' + match[1] + '\n' + stdout + '```',MessageType.text);
+      });
+}));
+}
