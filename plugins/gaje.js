9979bc0c8b83b44ffbc7743ec75e06417e0ac23e
mrclfd <mrclfd@yahoo.com>
2021-06-26 16:42:48 +0700

    Update gaje.js

--- plugins/gaje.js
+++ plugins/gaje.js
@@ -1,6 +1,4 @@
-/*
-DiCong-ed By @mrclfd
-*/
+// DiCong-ed By @mrclfd
  
 const Asena = require('../events');
 const { MessageType, Mimetype, } = require('@adiwajshing/baileys');
@@ -8,10 +6,6 @@ const con = require('../config');
 
 // Deskripsi
 const IDF = "Pesan gaje, buat yang gaje aja ( F || X || Y || 🗿 ).```\n⌨️ *Contoh:* ```.F 🌈"
-
-if (con.LANG === 'ID') {
-    
-    if (con.WORKTYPE === 'private') {
 
         Asena.addCommand({ pattern: 'F ?(.*)$', fromMe: true, desc: IDF }, async (message, match) => {
 
@@ -68,125 +62,4 @@ if (con.LANG === 'ID') {
             return await message.client.sendMessage(message.jid, fres, MessageType.text);
 
        });
-   }
-
  
- 
-else if (con.WORKTYPE === 'public') {
- 
-        Asena.addCommand({ pattern: 'F ?(.*)$', fromMe: false, desc: IDF }, async (message, match) => {
-
-            const fsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'FFFFFFFF\nFFFFFFFF\nFF\nFF\nFF\nFFFFFF\nFFFFFF\nFF\nFF\nFF\nFF\nFF', MessageType.text);
-
-            var str = "FFFFFFFF\nFFFFFFFF\nFF\nFF\nFF\nFFFFFF\nFFFFFF\nFF\nFF\nFF\nFF\nFF";
-  
-            var fres = str.replace(/F/g, `${fsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-
-        Asena.addCommand({ pattern: 'X ?(.*)$', fromMe: false, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'X               X\n  X           X\n    X       X\n      X   X\n         X\n      X   X\n    X       X\n  X           X\nX               X', MessageType.text);
-
-            var str = "X               X\n  X           X\n    X       X\n      X   X\n         X\n      X   X\n    X       X\n  X           X\nX               X";
-  
-            var fres = str.replace(/X/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-        
-        Asena.addCommand({ pattern: 'Y ?(.*)$', fromMe: false, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'Y                   Y\n    Y           Y    \n        Y   Y        \n            Y             \n            Y             \n            Y             \n            Y             ', MessageType.text);
-
-            var str = "Y                   Y\n    Y           Y    \n        Y   Y        \n            Y             \n            Y             \n            Y             \n            Y             ";
-  
-            var fres = str.replace(/Y/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-
- Asena.addCommand({ pattern: '🗿 ?(.*)$', fromMe: false, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, '👊🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👊🏿\n👉🏿👊🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👊🏾👈🏿\n👉🏿👉🏾👊🏽👇🏽👇🏽👇🏽👇🏽👇🏽👊🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👇🏼👇🏼👇🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👇🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👉🏻🗿👈🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👆🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👆🏼👆🏼👆🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👊🏽👆🏽👆🏽👆🏽👆🏽👆🏽👊🏽👈🏾👈🏿\n👉🏿👊🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👊🏾👈🏿\n👊🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👊🏿', MessageType.text);
-
-            var str = "👊🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👊🏿\n👉🏿👊🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👊🏾👈🏿\n👉🏿👉🏾👊🏽👇🏽👇🏽👇🏽👇🏽👇🏽👊🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👇🏼👇🏼👇🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👇🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👉🏻🗿👈🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👆🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👆🏼👆🏼👆🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👊🏽👆🏽👆🏽👆🏽👆🏽👆🏽👊🏽👈🏾👈🏿\n👉🏿👊🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👊🏾👈🏿\n👊🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👊🏿";
-  
-            var fres = str.replace(/🗿/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-       });
- 
- 
- // now private command can be run publicly
- 
- Asena.addCommand({ pattern: 'F ?(.*)$', fromMe: true, dontAddCommandList: true, desc: IDF }, async (message, match) => {
-
-            const fsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'FFFFFFFF\nFFFFFFFF\nFF\nFF\nFF\nFFFFFF\nFFFFFF\nFF\nFF\nFF\nFF\nFF', MessageType.text);
-
-            var str = "FFFFFFFF\nFFFFFFFF\nFF\nFF\nFF\nFFFFFF\nFFFFFF\nFF\nFF\nFF\nFF\nFF";
-  
-            var fres = str.replace(/F/g, `${fsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-
-        Asena.addCommand({ pattern: 'X ?(.*)$', fromMe: true, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'X               X\n  X           X\n    X       X\n      X   X\n         X\n      X   X\n    X       X\n  X           X\nX               X', MessageType.text);
-
-            var str = "X               X\n  X           X\n    X       X\n      X   X\n         X\n      X   X\n    X       X\n  X           X\nX               X";
-  
-            var fres = str.replace(/X/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-        
-        Asena.addCommand({ pattern: 'Y ?(.*)$', fromMe: true, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, 'Y                   Y\n    Y           Y    \n        Y   Y        \n            Y             \n            Y             \n            Y             \n            Y             ', MessageType.text);
-
-            var str = "Y                   Y\n    Y           Y    \n        Y   Y        \n            Y             \n            Y             \n            Y             \n            Y             ";
-  
-            var fres = str.replace(/Y/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-        });
-
- Asena.addCommand({ pattern: '🗿 ?(.*)$', fromMe: true, dontAddCommandList: true }, async (message, match) => {
-
-            const xsay = match[1]
-
-            if (match[1] === '') return await message.client.sendMessage(message.jid, '👊🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👊🏿\n👉🏿👊🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👊🏾👈🏿\n👉🏿👉🏾👊🏽👇🏽👇🏽👇🏽👇🏽👇🏽👊🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👇🏼👇🏼👇🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👇🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👉🏻🗿👈🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👆🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👆🏼👆🏼👆🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👊🏽👆🏽👆🏽👆🏽👆🏽👆🏽👊🏽👈🏾👈🏿\n👉🏿👊🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👊🏾👈🏿\n👊🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👊🏿', MessageType.text);
-
-            var str = "👊🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👊🏿\n👉🏿👊🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👊🏾👈🏿\n👉🏿👉🏾👊🏽👇🏽👇🏽👇🏽👇🏽👇🏽👊🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👇🏼👇🏼👇🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👇🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👉🏻🗿👈🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👉🏼👊🏻👆🏻👊🏻👈🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👉🏽👊🏼👆🏼👆🏼👆🏼👊🏼👈🏽👈🏾👈🏿\n👉🏿👉🏾👊🏽👆🏽👆🏽👆🏽👆🏽👆🏽👊🏽👈🏾👈🏿\n👉🏿👊🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👊🏾👈🏿\n👊🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👊🏿";
-  
-            var fres = str.replace(/🗿/g, `${xsay}`);
-        
-            return await message.client.sendMessage(message.jid, fres, MessageType.text);
-
-       });
-   }
-}
