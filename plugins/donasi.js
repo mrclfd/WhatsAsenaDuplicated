7a8086a0aecbff0eb4797dd5e07962439cfccf70
mrclfd <mrclfd@yahoo.com>
2021-06-26 17:20:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 const Language = require('../language');
 const Lang = Language.getString('donasi');
   
-  if (con.WORKTYPE == 'private') {
-  
     Asena.addCommand({pattern: 'donasi$', fromMe: true, desc: Lang.DONASI_DESC }, (async (message, match) => {
       
           var image = await axios.get ('https://raw.githubusercontent.com/mrclfd/WhatsAsenaDuplicated/alpha-test/media/image/image-i.png', {responseType: 'arraybuffer'})
@@ -22,37 +20,3 @@ const Lang = Language.getString('donasi');
      */
   
         }));
-  }
-  else if (con.WORKTYPE == 'public') {
-  
-    Asena.addCommand({pattern: 'donasi$', fromMe: false, desc: Lang.DONASI_DESC }, (async (message, match) => {
-      
-          var image = await axios.get ('https://raw.githubusercontent.com/mrclfd/WhatsAsenaDuplicated/alpha-test/media/image/image-i.png', {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Buy Me a Coffee\nvia* saweria.co/budilasidodo`})
-      
-     /*
-        await new Promise(r => setTimeout(r, 1650));
-  
-        await message.sendMessage('Thx u <3');
-     */
-  
-        }));
-    
-    
-    // now private command can be run publicly
-    
-    Asena.addCommand({pattern: 'donasi$', fromMe: false, dontAddCommandList: true, desc: Lang.DONASI_DESC }, (async (message, match) => {
-      
-          var image = await axios.get ('https://raw.githubusercontent.com/mrclfd/WhatsAsenaDuplicated/alpha-test/media/image/image-i.png', {responseType: 'arraybuffer'})
-       
-        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Buy Me a Coffee\nvia* saweria.co/budilasidodo`})
-      
-     /*
-        await new Promise(r => setTimeout(r, 1650));
-  
-        await message.sendMessage('Thx u <3');
-     */
-  
-        }));
-  }  
