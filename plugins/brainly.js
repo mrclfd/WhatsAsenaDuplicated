98e1ef51c94cb3dc6c5e09f968d6313dbfae5b38
mrclfd <mrclfd@yahoo.com>
2021-06-26 17:29:16 +0700

    Update brainly.js

--- plugins/brainly.js
+++ plugins/brainly.js
@@ -6,8 +6,6 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('brainly');
 
-if (Config.WORKTYPE == 'private') {
-
 Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: true, desc: Lang.BRAINLY_DESC }, async (message, match) => {
 
         const Soal = match[1]
@@ -28,52 +26,3 @@ Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: true, desc: Lang.BRAINLY_DE
            })
       },
     )
-}
-
-if (Config.WORKTYPE == 'public') {
-  
-  Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: false, desc: Lang.BRAINLY_DESC }, async (message, match) => {
-
-        const Soal = match[1]
-        
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'*Masukkan pertanyaan!*', MessageType.text);
-
-        await axios
-          .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
-          .then(async (response) => {
-            const {
-              soal,
-              jawaban,
-            } = response.data
-
-            const msg = `*Soal:* ${soal}
-*Jawaban Brainly:* ${jawaban.replace(/1Pertanyaan/g, '*(1) Pertanyaan*').replace(/2Pertanyaan/g, '*(2) Pertanyaan*').replace(/3Pertanyaan/g, '*(3) Pertanyaan*').replace(/Jawaban/g, '*Jawaban*').replace(/Brainly ditemukan/g, '')}`
-            await message.client.sendMessage(message.jid, msg, MessageType.text)
-           })
-      },
-    )
-        
-        
- // now private command can be run publicly
-        
-        Asena.addCommand({ pattern: 'brainly ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.BRAINLY_DESC }, async (message, match) => {
-
-        const Soal = match[1]
-        
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'*Masukkan pertanyaan!*', MessageType.text);
-
-        await axios
-          .get(`https://api.xteam.xyz/brainly?APIKEY=10c4105200edc0f0&soal=${Soal}`)
-          .then(async (response) => {
-            const {
-              soal,
-              jawaban,
-            } = response.data
-
-            const msg = `*Soal:* ${soal}
-*Jawaban Brainly:* ${jawaban.replace(/1Pertanyaan/g, '*(1) Pertanyaan*').replace(/2Pertanyaan/g, '*(2) Pertanyaan*').replace(/3Pertanyaan/g, '*(3) Pertanyaan*').replace(/Jawaban/g, '*Jawaban*').replace(/Brainly ditemukan/g, '')}`
-            await message.client.sendMessage(message.jid, msg, MessageType.text)
-           })
-      },
-    )
-}
