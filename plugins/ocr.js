96c9fdf42ee43518e6134f991c446761779476a1
mrclfd <mrclfd@yahoo.com>
2021-06-26 16:14:57 +0700

    Update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -15,8 +15,6 @@ const langs = require('langs');
 const Language = require('../language');
 const Lang = Language.getString('ocr');
 
-if (Config.WORKTYPE == 'private') {
-
     Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => { 
 
         if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
@@ -51,79 +49,3 @@ if (Config.WORKTYPE == 'private') {
 
         return await message.reply(Lang.RESULT.format(dil[2], result));
     }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-    Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: false, desc: Lang.OCR_DESC}, (async (message, match) => { 
-
-        if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
-	var info = await message.reply(Lang.DOWNLOADING);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-
-        var dil;
-        if (match[1] !== '') {
-            dil = langs.where("1", match[1]);
-        } else {
-            dil = langs.where("1", Config.LANG.toLowerCase());
-        }
-
-        try {
-            var result = await tesseract.recognize(location, {
-                lang: dil[2]
-            });    
-        } catch (e) {
-            return await message.reply(Lang.ERROR.format(e));
-        }
-
-        await info.delete();
-        if ( result === ' ' || result.length == 1 ) {
-            return await message.reply(Lang.ERROR.format(' Empty text'));
-        }
-
-        return await message.reply(Lang.RESULT.format(dil[2], result));
-    }));
-	
-	
-	// now private command can be run publicly
-	
-	Asena.addCommand({pattern: 'ocr ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.OCR_DESC}, (async (message, match) => { 
-
-        if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);    
-	var info = await message.reply(Lang.DOWNLOADING);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-
-        var dil;
-        if (match[1] !== '') {
-            dil = langs.where("1", match[1]);
-        } else {
-            dil = langs.where("1", Config.LANG.toLowerCase());
-        }
-
-        try {
-            var result = await tesseract.recognize(location, {
-                lang: dil[2]
-            });    
-        } catch (e) {
-            return await message.reply(Lang.ERROR.format(e));
-        }
-
-        await info.delete();
-        if ( result === ' ' || result.length == 1 ) {
-            return await message.reply(Lang.ERROR.format(' Empty text'));
-        }
-
-        return await message.reply(Lang.RESULT.format(dil[2], result));
-    }));
-}
