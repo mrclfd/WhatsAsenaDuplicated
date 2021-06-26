93c4ce510beb9f787dece96dbb7938ffd9b729db
mrclfd <mrclfd@yahoo.com>
2021-06-26 12:10:21 +0700

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -10,7 +10,6 @@ const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
-if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
@@ -23,32 +22,3 @@ if (Config.WORKTYPE == 'private') {
         });
         await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }));
-}
-else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
-        let grup = await message.client.groupMetadata(message.jid);
-        var jids = [];
-        mesaj = '';
-        grup['participants'].map(async (uye) => {
-            if (uye.isAdmin) {
-                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
-                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-            }
-        });
-        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-    }));
-    
-    // now private command can be run publicly
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
-        let grup = await message.client.groupMetadata(message.jid);
-        var jids = [];
-        mesaj = '';
-        grup['participants'].map(async (uye) => {
-            if (uye.isAdmin) {
-                mesaj += '▫️@' + uye.id.split('@')[0] + '\n';
-                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-            }
-        });
-        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
-    }));
-}
