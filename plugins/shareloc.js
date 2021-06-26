de5cdabfba056b2df948b1939b214bd97b9f0b5b
mrclfd <mrclfd@yahoo.com>
2021-06-26 13:08:13 +0700

    Update shareloc.js

--- plugins/shareloc.js
+++ plugins/shareloc.js
@@ -9,8 +9,6 @@ const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('locate'); // Language supp. 😉
-
-if (Config.WORKTYPE == 'private') {
     
     Asena.addCommand({pattern: 'loc', fromMe: true, desc: Lang.L_DESC, warn: Lang.L_WARN}, (async (message, match) => {
 
@@ -23,22 +21,3 @@ if (Config.WORKTYPE == 'private') {
         await message.sendMessage(`My Location! ${r_text[i]}`, MessageType.location); // It sends ur location. Cool tho 😱
 
 }));
-}
-
-if (Config.WORKTYPE == 'public') {
-    
-    
-    // now private command can be run publicly
-
-    Asena.addCommand({pattern: 'loc', fromMe: true, dontAddCommandList: true, desc: Lang.L_DESC, warn: Lang.L_WARN}, (async (message, match) => {
-
-        var r_text = new Array ();
-        r_text[0] = "degreesLatitude: 24.121231, degreesLongitude: 55.1121221"; // Actually, I don't know where is this place..
-        r_text[1] = "degreesLatitude: 8.838637, degreesLongitude: -13.721434"; // U too homie
-
-        var i = Math.floor(2*Math.random()) // Random func. 🤪
-
-        await message.sendMessage(`My Location! ${r_text[i]}`, MessageType.location); // It sends ur location. Cool tho 😱
-
-}));
-}
