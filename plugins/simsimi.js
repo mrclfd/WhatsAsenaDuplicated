04612ea0cd46bbd4661c62c9576684485d3a0c4b
mrclfd <mrclfd@yahoo.com>
2021-06-27 14:01:21 +0700

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -3,17 +3,20 @@ const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 
-const Language = require('../language');
-
-    Asena.addCommand({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
-	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
+const SIMI_DESC = "Chat with an AI Bot.\n Use .bot <message>"
+const NEED_LOCATION = "*Invalid Request*"
+const BOT = ":"
+const NOT_FOUND = "*Invalid Request*"
+		   
+    Asena.addCommand({pattern: 'bot ?(.*)', fromMe: true, desc: SIMI_DESC}, async (message, match) => {
+	if (match[1] === 'xx') return await message.reply(NEED_LOCATION);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
-	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n\n*BOT* 🤠 ' + Lang.BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+	  if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n\n*BOT* 🤠 ' + BOT +' ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
 	} catch {
-		return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
+		return await message.client.sendMessage(message.jid, NOT_FOUND, MessageType.text);
 	}
     });
     
