86c607c3169175e1248873e26814c2491014b01b
mrclfd <mrclfd@yahoo.com>
2021-06-27 15:00:31 +0700

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -13,6 +13,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 const Language = require('../language');
 const Lang = Language.getString('_asena');
 
+/*
     Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
@@ -38,6 +39,62 @@ const Lang = Language.getString('_asena');
                     if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
                     if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
 
+                }
+            );
+
+            await message.client.sendMessage(
+                message.jid,'*Please specify which command do you want help for !!*\n*Usage:* ```.help <command name>```\n*List for all available command below:* \n\n' + CMD_HELP, MessageType.text
+            );    
+        } else {
+
+            var CMD_HELP = '';
+            Asena.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                    } catch {
+                        var cmatch = [command.pattern];
+                    }
+                
+                    if (cmatch[2] == match[1]) {
+                        var HANDLER = '';
+    
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                        } else {
+                            HANDLER = '.';
+                        }
+                        CMD_HELP += '*♠️ ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
+                        if (command.desc !== '') CMD_HELP += '*ℹ️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
+                        if (command.usage !== '') CMD_HELP += '*⌨️ ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+
+                    }
+                }
+            );
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+            await message.client.sendMessage(
+                message.jid,'*Command Usage*:\n\n' + CMD_HELP, MessageType.text
+            );
+        }
+    }));
+*/
+
+Asena.addCommand({pattern: 'help ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+
+        var CMD_HELP = '';
+        if (match[1] === '') {
+            Asena.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşiö ç1234567890]*)/);
+                    } catch {
+                        var match = [command.pattern];
+                    }
+   
+                    CMD_HELP += '```' + (match.length >= 3 ? (match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
                 }
             );
 
