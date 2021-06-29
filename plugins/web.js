8540e92fccd28b41f5f3c839d80a2be6fb3f7ba8
mrclfd <mrclfd@yahoo.com>
2021-06-29 10:36:05 +0700

    Update web.js

--- plugins/web.js
+++ plugins/web.js
@@ -43,39 +43,11 @@ Asena.addCommand({pattern: 'speed', fromMe: true, desc: Lang.SPEEDTEST_DESC}, (a
     await msg.delete();
 }));
 
-Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: false, desc: Lang.PING_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'ping', fromMe: true, deleteCommand: true, desc: Lang.PING_DESC}, (async (message, match) => {
   var start = new Date().getTime();
-  await message.sendMessage('```Ping!```');
+// await message.sendMessage('```Ping!```');
   var end = new Date().getTime();
 
   await message.client.sendMessage(
-    message.jid,'*Pong!*\n```' + (end - start) + 'ms```', MessageType.text);
+    message.jid,'*Pong!*\n```' + (end - start) + 'ms```', MessageType.text, {quoted: "```Ping!```" });
 }));
-
-Asena.addCommand({pattern: 'calc ?(.*)', fromMe: true, desc: Lang.CALC }, (async (message, match) => {
-        if (match[1].length < 4) { return await message.client.sendMessage(message.jid,Lang.VALİD, MessageType.text) }
-        if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
-            var result = -(-ilksayi - sonsayi)
-            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
-            }
-        }
-        else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
-            var result = ilksayicik - sonsayicik
-            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
-            }
-        }
-        else if (match[1].includes('x')) { var split = match[1].split('x'), sonsayicarp = split[1], ilksayicarp = split[0] 
-            var result = ilksayicarp * sonsayicarp
-            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text);
-            }
-        }
-        else if (match[1].includes('/')) { var split = match[1].split('/'), sonsayibol = split[1], ilksayibol = split[0] 
-            var result = ilksayibol / sonsayibol
-            try { await message.client.sendMessage(message.jid,Lang.SUC + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid,Lang.UNSUC + err,MessageType.text)
-            }
-        }
-    }));
