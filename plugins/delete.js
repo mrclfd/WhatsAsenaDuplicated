a842fcb953866fe0ee773a9e5fe47ae7dcbdcd44
mrclfd <mrclfd@yahoo.com>
2021-06-29 08:50:44 +0700

    Create delete.js

---
+++ plugins/delete.js
@@ -0,0 +1,9 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const DEL_DESC = "Deletes The Replied Message Send By The Bot."
+
+Asena.addCommand({pattern: 'del', fromMe: false && true, desc: DEL_DESC}, (async (message, match) => {
+
+  await message.reply_message.delete();
+
+}));
