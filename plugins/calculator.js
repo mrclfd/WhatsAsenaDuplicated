e1c75452954f7d28a38f1b2c59b03a3bd651944e
mrclfd <mrclfd@yahoo.com>
2021-06-29 13:57:08 +0700

    Update calculator.js

--- plugins/calculator.js
+++ plugins/calculator.js
@@ -15,29 +15,29 @@ const gagal = "*Perhitungan Gagal ❌*\n*Kesalahan:* \n"
 
 
 Asena.addCommand({pattern: 'calc ?(.*)', fromMe: true, desc: calc }, (async (message, match) => {
-        if (match[1].length < 4) { return await message.client.sendMessage(message.jid, valid, MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }}) }
+        if (match[1].length < 4) { return await message.client.sendMessage(message.jid, valid, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }}) }
         if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
             var result = -(-ilksayi - sonsayi)
-            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }});
+            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }}) }
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }});
             }
         }
         else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
             var result = ilksayicik - sonsayicik
-            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }});
+            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }}) }
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }});
             }
         }
         else if (match[1].includes('×')) { var split = match[1].split('×'), sonsayicarp = split[1], ilksayicarp = split[0] 
             var result = ilksayicarp * sonsayicarp
-            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }});
+            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }}) }
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }});
             }
         }
         else if (match[1].includes(':')) { var split = match[1].split(':'), sonsayibol = split[1], ilksayibol = split[0] 
             var result = ilksayibol / sonsayibol
-            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 10, isForwarded: true }})
+            try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }}) }
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }})
             }
         }
     }));
