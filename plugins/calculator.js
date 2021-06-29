2baaf8506c5c64efa6263e51802b7fd46d6075b7
mrclfd <mrclfd@yahoo.com>
2021-06-29 13:04:18 +0700

    Update calculator.js

--- plugins/calculator.js
+++ plugins/calculator.js
@@ -1,4 +1,3 @@
-  
 /* Copyright (C) 2020 Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
@@ -10,35 +9,35 @@ const {MessageType} = require('@adiwajshing/baileys');
 
 // CMD_HELP
 const calc = "Menghitung layaknya kalkulator sederhana."
-const valid = "*Masukkan format dengan benar!*\n\nSeperti ini:\n.calc 666+13\n.calc 666×13\n.calc 666:13\n.calc 666-13"
+const valid = "*Masukkan format dengan benar!*\n\n*Seperti ini:*\n.calc 666 + 13\n.calc 666 × 13\n.calc 666 : 13\n.calc 666 - 13"
 const sukses = "*Perhitungan Selesai ✅*\n*Hasil:* "
 const gagal = "*Perhitungan Gagal ❌*\n*Kesalahan:* \n"
 
 
 Asena.addCommand({pattern: 'calc ?(.*)', fromMe: true, desc: calc }, (async (message, match) => {
-        if (match[1].length < 4) { return await message.client.sendMessage(message.jid, valid, MessageType.text) }
+        if (match[1].length < 4) { return await message.client.sendMessage(message.jid, valid, MessageType.text, {contextInfo: { forwardingScore: 3, isForwarded: true }}) }
         if (match[1].includes('+')) { var split = match[1].split('+'), sonsayi = split[1], ilksayi = split[0]
             var result = -(-ilksayi - sonsayi)
             try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text);
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text, {contextInfo: { forwardingScore: 3, isForwarded: true }});
             }
         }
         else if (match[1].includes('-')) { var split = match[1].split('-'), sonsayicik = split[1], ilksayicik = split[0] 
             var result = ilksayicik - sonsayicik
             try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text);
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text, {contextInfo: { forwardingScore: 3, isForwarded: true }});
             }
         }
         else if (match[1].includes('×')) { var split = match[1].split('×'), sonsayicarp = split[1], ilksayicarp = split[0] 
             var result = ilksayicarp * sonsayicarp
             try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text);
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text, {contextInfo: { forwardingScore: 3, isForwarded: true }});
             }
         }
         else if (match[1].includes(':')) { var split = match[1].split(':'), sonsayibol = split[1], ilksayibol = split[0] 
             var result = ilksayibol / sonsayibol
             try { await message.client.sendMessage(message.jid, sukses + result, MessageType.text) }
-            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text)
+            catch (err) { return await message.client.sendMessage(message.jid, gagal + err,MessageType.text, {contextInfo: { forwardingScore: 3, isForwarded: true }})
             }
         }
     }));
